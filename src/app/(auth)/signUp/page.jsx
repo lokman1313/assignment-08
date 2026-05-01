07e61faabb91541cc3130c496e69338b6931aa3f
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-05-01 13:48:03 +0600

    resister page backend complite

--- src/app/(auth)/signUp/page.jsx
+++ src/app/(auth)/signUp/page.jsx
@@ -1,26 +1,55 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+
 
 
 const SignUpPage = () => {
+
+const { register, handleSubmit, formState: { errors },} = useForm();
+   
+const handelRegForm = async (formData) => {
+    const {name,image,email,password}=formData;
+    const { data, error } = await authClient.signUp.email({
+    name: name, // required
+    email:email, // required
+    password:password, // required
+    image: image,
+    callbackURL: "/",
+});
+if (error) {
+    console.log(error);
+    alert(error.message);
+  } else {
+    console.log(data);
+  }
+}
+
     return (
         <div className="my-20">
+            <form onSubmit={handleSubmit(handelRegForm)}>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs mx-auto  border p-4">
   <h2 className="text-center text-3xl font-semibold">Create Account</h2>
   <p className="text-center font-semibold">Register to get started</p>
 
   <label className="label">Name</label>
-  <input type="text" className="input" placeholder="Enter Your Name" />
+  <input type="text" className="input" placeholder="Enter Your Name" {...register("name",{ required: "name is required" })}/>
+  {errors.name && <p className="text-red-500 text-xs mt-1">{errors.name.message}</p>}
 
   <label className="label">Image URL</label>
-  <input type="text" className="input" placeholder="Enter Your Image URL" />
+  <input type="text" className="input" placeholder="Enter Your Image URL" {...register("image")} />
 
   <label className="label">Email</label>
-  <input type="email" className="input" placeholder="Enter Your Email" />
+  <input type="email" className="input" placeholder="Enter Your Email" {...register("email",{ required: "email is required" })}/>
+  {errors.email && <span className="text-red-500">{errors.email.message}</span>}
 
   <label className="label">Password</label>
-  <input type="password" className="input" placeholder="Enter Your Password" />
+  <input type="password" className="input" placeholder="Enter Your Password"{...register("password",{ required: "password is required" })} />
+  {errors.password && <span className="text-red-500">{errors.password.message}</span>}
 
   <button className="btn btn-neutral mt-4">Register</button>
+
   <div className="divider">OR</div>
   <button className="btn bg-white text-black border-[#e5e5e5]">
   <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
@@ -28,7 +57,8 @@ const SignUpPage = () => {
 </button>
 
 <p className="text-center">Allready have an account ? <span className="text-blue-400"><Link href={"/login"}>Login</Link></span></p>
-</fieldset>
+        </fieldset>
+            </form>
         </div>
     );
 };
--- src/app/(main)/profile/page.jsx
+++ src/app/(main)/profile/page.jsx
@@ -1,9 +1,26 @@
-
+import Image from "next/image";
+import hero from "../../../../public/hero-bg.jpg"
+const user={
+    name:"Md.lokman Hossen",
+    id:102,
+    email:"lokman@gmail.com"
+    
+}
 
 const ProfilePage = () => {
     return (
         <div className="container mx-auto ">
             <h2 className="text-3xl font-bold">My Profile</h2>
+            <div className="bg-base-200 rounded-3xl w-1/2 mx-auto flex flex-col justify-center items-center py-20 ">
+              <Image src={hero} width={250} alt="hero" height={100}
+              className="rounded-full "
+              ></Image>
+              <div className="mt-15">
+                <h3 className="  text-gray-500">User ID : {user.id}</h3>
+                <h3 className="text-lg text-gray-600">Name : {user.name}</h3>
+                <h3 className="text-sm text-gray-600">Emsil : {user.email}</h3>
+              </div>
+            </div>
         </div>
     );
 };
