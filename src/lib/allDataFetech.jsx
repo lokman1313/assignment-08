a4f8d2a448362a1328da9c14dad51b33857375d7
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-05-01 21:48:49 +0600

    error handelin

--- src/lib/allDataFetech.jsx
+++ src/lib/allDataFetech.jsx
@@ -1,5 +1,10 @@
-export const products = async ()=>{
- const res = await fetch("http://localhost:5000/products")
- const data = await res.json()
- return data
+// export const products = async ()=>{
+//  const res = await fetch("http://localhost:5000/products")
+//  const data = await res.json()
+//  return data
+// }
+import db from '../../db.json'
+
+export const products = async () => {
+  return db.products
 }
