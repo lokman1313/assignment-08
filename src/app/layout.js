018d9f73a5002de6b632978a84b8215416820564
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-05-02 12:01:44 +0600

    react tostifi containar impliment on main layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import NavbarPage from "@/components/NavbarPage";
+import { ToastContainer } from "react-toastify";
 
 
 const geistSans = Geist({
@@ -28,7 +29,10 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <NavbarPage></NavbarPage>
-        <main>{children}</main>
+        <main>
+          {children}
+          <ToastContainer />
+        </main>
         <Footer></Footer>
         </body>
     </html>
