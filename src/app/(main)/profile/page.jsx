da54e3fef7a0d9172ce7276a0abf22cb351f3355
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-05-02 10:37:04 +0600

    profile page updatede

--- src/app/(main)/profile/page.jsx
+++ src/app/(main)/profile/page.jsx
@@ -4,6 +4,7 @@ import UpdateProfile from "@/components/UpdateProfile";
 import { authClient } from "@/lib/auth-client";
 import Image from "next/image";
 
+
 const ProfilePage = () => {
     
     const { data: session, isPending } = authClient.useSession();
@@ -23,13 +24,11 @@ const ProfilePage = () => {
             <h2 className="text-3xl font-bold mt-10 ml-10">My Profile</h2>
             <div className="bg-base-200 rounded-3xl md:w-1/3 mx-auto flex flex-col justify-center items-center py-20 mt-10">
                 
-                <Image 
-                    src={userData?.image} 
-                    width={150} 
-                    alt="profile-picture" 
-                    height={100}
-                    className="rounded-full shadow-lg"
-                />
+
+                <div className="ring-success ring-offset-base-100 rounded-full ring-2 ring-offset-2">
+                <Image src={userData.image} alt="logo" width={50} height={32}></Image>
+                </div>
+                
                 
                 <div className="my-6 text-center">
                     <h3 className="text-gray-500">User ID: {userData?.id}</h3>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10 mt-20">
   <nav className="grid grid-flow-col gap-4">
     <a className="link link-hover">About us</a>
-    <a href="mailto:yourmail@gmail.com" className="link link-hover">Contact</a>
+    <a href="mailto:lokmant733@gmail.com" className="link link-hover">Contact</a>
     <a className="link link-hover">Jobs</a>
     <a className="link link-hover">Press kit</a>
   </nav>
